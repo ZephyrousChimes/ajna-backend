49eaa7e258d22f1fba28eaddc592a81f77e9e8bc
= <=>
2025-04-07 06:30:17 +0530

    Project set up

---
+++ src/main/java/org/dexteradei/whitegold/WhitegoldApplication.java
@@ -0,0 +1,13 @@
+package org.dexteradei.whitegold;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhitegoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhitegoldApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/dexteradei/whitegold/WhitegoldApplicationTests.java
@@ -0,0 +1,13 @@
+package org.dexteradei.whitegold;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhitegoldApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
